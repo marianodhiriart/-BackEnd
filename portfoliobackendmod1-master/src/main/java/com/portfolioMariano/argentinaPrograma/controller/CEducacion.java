100064d999f7150160ab20651c71672bbae77ba4
marianodhiriart <109999363+marianodhiriart@users.noreply.github.com>
2023-04-20 14:44:10 -0300

    Corrección del crossOrigin

--- portfoliobackendmod1-master/src/main/java/com/portfolioMariano/argentinaPrograma/controller/CEducacion.java
+++ portfoliobackendmod1-master/src/main/java/com/portfolioMariano/argentinaPrograma/controller/CEducacion.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("educacion")
-@CrossOrigin(origins = "https://ejemplo2-ejemplo2.web.app/")
+@CrossOrigin(origins = "https://ejemplo2-ejemplo2.web.app")
 public class CEducacion {
     @Autowired
     SEducacion educacion;
